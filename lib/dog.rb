f0db3847ffab256ea638403b4ca91e8a01b907cf
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-17 18:53:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,14 @@
 class Dog 
-  def name= (call_dog_by)
-    @name 
+  def name= (new_dog)
+    @name = new_dog
+    end
+  def name
+    @name
+    end 
+  def breed= (dog_breed) 
+    @breed = dog_breed
+  end 
+  def breed
+    @breed
   end 
 end 
