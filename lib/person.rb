c7ecba94f7e2041da279a2b34225212c51368501
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-06-17 18:56:22 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-
+  
+  def person= (this_is_you)
+  @person = this_is_you
+  end
+  def person
+    @person
+  end
+  def job= (person_works_here)
+    @job = person_works_here
+  end 
+  def job
+    @job
+  end 
 end
